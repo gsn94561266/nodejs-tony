af83cdd86087834a63f2a59dfd71b9ab4413ee93
gsn94561266 <gsn94561266@gmail.com>
2022-12-11 15:55:29 +0800

    update readFile

--- crawler/crawler3.js
+++ crawler/crawler3.js
@@ -1,9 +1,3 @@
-// axios await 版本
-// 把 query string 抽出來當變數，用 params 的方式去設定
-
-// 1. 安裝 npm i axios
-// 2. 引用 require
-// 3. 去讀官方文件
 const fs = require("fs");
 const axios = require("axios");
 // http://54.71.133.152:3000/stocks?stockNo=2618&date=202211
