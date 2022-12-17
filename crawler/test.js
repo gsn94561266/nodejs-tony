0b00a5b2b3cbb7e5509fea212e6e44b90d91ce1f
gsn94561266 <gsn94561266@gmail.com>
2022-12-17 11:38:40 +0800

    update 202212171138

---
+++ crawler/test.js
@@ -0,0 +1,18 @@
+const mysql2 = require("mysql2/promise");
+
+(async () => {
+  const connection = await mysql2.createConnection({
+    host: "localhost",
+    user: "admin",
+    password: "",
+    database: "stock_mfee31",
+  });
+
+  // simple query
+  let result = await connection.query("SELECT * FROM `stocks`");
+  let data = result[0];
+  // console.log(result);
+  console.log(data);
+
+  connection.end();
+})();
