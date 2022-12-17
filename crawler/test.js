213258c2c962998cea2969f51b575c81a1be205e
gsn94561266 <gsn94561266@gmail.com>
2022-12-17 11:58:16 +0800

    update 20221217

--- crawler/test.js
+++ crawler/test.js
@@ -1,16 +1,22 @@
+// install mysql2
 const mysql2 = require("mysql2/promise");
+
+// npm i dotenv
+require("dotenv").config();
+// let dotenv = require('dotenv');
+// dotenv.config();
 
 (async () => {
   const connection = await mysql2.createConnection({
-    host: "localhost",
-    user: "admin",
-    password: "",
-    database: "stock_mfee31",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    database: process.env.DB_NAME,
   });
 
   // simple query
-  let result = await connection.query("SELECT * FROM `stocks`");
-  let data = result[0];
+  let [data, fields] = await connection.query("SELECT * FROM `stocks`");
   // console.log(result);
   console.log(data);
 
