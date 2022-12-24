cf4d825a891be8ed94ac421ed21ddbc78c54c521
gsn94561266 <gsn94561266@gmail.com>
2022-12-24 14:52:40 +0800

    202212240252

--- stock-be/server.js
+++ stock-be/server.js
@@ -1,20 +1,45 @@
-const { request, response } = require("express");
-const express = require("express");
-//利用 express 這個框架建立一個 web app
+const express = require('express');
+// 利用 express 這個框架建立一個 web app
 const app = express();
+
+require('dotenv').config();
+const mysql2 = require('mysql2/promise');
+
+let pool = mysql2.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PWD,
+  database: process.env.DB_NAME,
+  // 限制 pool 連線數的上限
+  connectionLimit: 10,
+});
+
+// 允許跨源存取
+// 預設是全部開放
+// 也可以做部分限制，參考 npm cors 的文件
+const cors = require('cors');
+app.use(cors());
 
 // middleware => pipeline pattern
 
+// 設定 express 處理靜態檔案
+// -> express 內建 -> 不需要安裝任何東西
+// localhost:3001/
+// app.use(express.static('./static'));
+// localhost:3001/2048/
+app.use('/2048', express.static('./static'));
+
 // 中間件
 app.use((req, res, next) => {
-  console.log("這裡是一個中間件 A");
-  req.mfee31 = "水母班";
+  console.log('這裡是的一個中間件 A');
+  req.mfee31 = '水母班';
   next();
-  // res.send("這裡是 A 中間件");
+  // res.send('這裡是 A 中間件');
 });
 
 app.use((req, res, next) => {
-  console.log("這裡是一個中間件 B");
+  console.log('這裡是的一個中間件 B');
   req.dt = new Date().toISOString();
   next();
 });
@@ -22,30 +47,45 @@ app.use((req, res, next) => {
 // app.[Method]
 // get, post, put, patch, delete, option, head
 // 路由中間件
-app.get("/", (req, res) => {
-  console.log("這裡是首頁 2", req.mfee31, req.dt);
-  res.send("Hello Express 9");
+app.get('/', (req, res, next) => {
+  console.log('這裡是首頁 2', req.mfee31, req.dt);
+  res.send('Hello Express 9');
+});
+
+app.get('/api', (req, res, next) => {
+  res.json({
+    name: 'John',
+    age: 18,
+  });
+});
+
+app.get('/api/stocks', async (req, res, next) => {
+  // let results = await connection.query('SELECT * FROM stocks');
+  // let data = results[0];
+  console.log('這裡是 /api/stocks');
+  let [data] = await pool.query('SELECT * FROM stocks');
+  res.json(data);
 });
 
 app.use((req, res, next) => {
-  console.log("這裡是一個中間件 C");
+  console.log('這裡是的一個中間件 C');
   next();
 });
 
-app.get("/test", (req, res, next) => {
-  console.log("這裡 test 頁面", req.dt);
-  res.send("Hello Test 1");
+app.get('/test', (req, res, next) => {
+  console.log('這裡是 test 頁面', req.dt);
+  res.send('Hello Test 1');
 });
 
-// 放在所有"路由中間件"的後面
-// 前面所有路由器都比對不到網址時，就會跑到這裡
+// 放在所有的路由中間件的後面
+// 前面所有的路由都比不到對的網址時，就會掉到這裡來
 // --> 這就是一個 404 的情況
-// 利用了中間件會依照程式碼順序執行的特性
+// 利用了中間件會依照程式碼順序來執行的特性
 app.use((req, res, next) => {
-  console.log("這裡是 404");
-  res.send("沒有這個網頁啦");
+  console.log('這裡是 404');
+  res.send('沒有這個網頁啦');
 });
 
 app.listen(3001, () => {
-  console.log("server running at port 3001");
+  console.log('Server running at port 3001');
 });
---
+++ stock-fe/src/components/Stock.js
@@ -0,0 +1,57 @@
+import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Stock = () => {
+  const [error, setError] = useState(null);
+  const [stocks, setStocks] = useState([]);
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log('沒有第二個參數');
+  });
+
+  useEffect(() => {
+    console.log('第二個參數是空陣列');
+    // 在 component 初始化的時候跑一次
+    // 通常會把去跟後端要資料的動作放在這裡
+    async function getStocks() {
+      let response = await axios.get('http://localhost:3001/api/stocks');
+      setStocks(response.data);
+    }
+    getStocks();
+  }, []);
+
+  useEffect(() => {
+    console.log('第二個參數是counter');
+  }, [counter]);
+
+  return (
+    <div>
+      {error && <div>{error}</div>}
+      <h2 className="ml-7 mt-6 text-xl text-gray-600">股票代碼</h2>
+
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+        }}>
+        add {counter}
+      </button>
+
+      {stocks.map((stock) => {
+        return (
+          <div className="bg-white bg-gray-50 p-6 rounded-lg shadow hover:shadow-lg m-6 cursor-pointer">
+            <Link to={'/stock/1234'}>
+              <h2 className="text-2xl font-bold mb-2 text-gray-800">
+                {stock.id}
+              </h2>
+              <p className="text-gray-700">{stock.name}</p>
+            </Link>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Stock;
